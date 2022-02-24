c008d9faa6caa743f2ad75a1372aeb318a669710
Ricardo Garcia <81274447+RickyGeez@users.noreply.github.com>
2022-02-24 11:58:42 -0800

    A3 Exercise 3 Update

--- app.js
+++ app.js
@@ -2,12 +2,25 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var credentials = [];
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/demo', (req, res) => {
   res.send('This is a nodejs demo!')
+})
+
+app.get('/credentials', (req, res) => {
+    var credentialsRecord = {
+        'FirstName': req.query.firstName,
+        'LastName': req.query.lastName,
+        'Age': req.query.age
+    };
+    credentials.push(credentialsRecord);
+    console.log(credentials);
+    res.send('Credentials have been added!');
 })
 
 app.listen(port, () => {
