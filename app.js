6d8582613dfb828e6d591c89c4e511ec37c2fe95
viksirivong <vsirivong@yahoo.com>
2022-03-01 23:36:10 -0800

    Updated app.js for assignment 4

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import {compareAsc, format} from 'date-fns'
 import chalk from 'chalk';
 import express from 'express';
 const app = express();
@@ -38,6 +39,19 @@ app.get('/underscore', (req, res) => {
 
 app.get('/members', (req, res) => {
   res.send('Team Members: Derek Hoang, Ricardo Garcia, Gaia Dennison, Thongsavik Sirivong')
+})
+
+app.get('/dateDemo', (req, res) => {
+  format(new Date(2014, 1, 11), 'yyyy-MM-dd')
+
+  const dates = [
+    new Date(1995, 6, 2),
+    new Date(1987, 1, 11),
+    new Date(1989, 6, 10),
+  ]
+  dates.sort(compareAsc)
+
+  res.send(dates)
 })
 
 app.listen(port, () => {
