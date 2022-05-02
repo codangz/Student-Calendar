9a6d70a5aa5b83264294077523641e2055c52979
Derek Hoang <derekhoang@rocketmail.com>
2022-05-02 15:58:51 -0700

    Fixed Calendar and updated version of libraries

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "http://localhost:5000"
 };
 
 app.use(cors(corsOptions));
@@ -42,7 +42,7 @@ require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
